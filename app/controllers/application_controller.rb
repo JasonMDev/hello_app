82e85b3bd7d0539b2693953d9fdfe905a9007a00
JasonMDev <jasonmansdev@gmail.com>
2015-11-07 15:46:15 +0000

    Modified apllication view and controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
 
   def hello
   	render text: "Hello World!"
-  	render text: "#{'#'][]''}Hello World!"
-
-
+  	render text: "BGGgfgHello World!"
   end
+
 end
